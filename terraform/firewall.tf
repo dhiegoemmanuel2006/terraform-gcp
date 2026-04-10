71f6601c982ecf479bf4dac79156add902781156
Dhiego Andrade <dhiegoemmanuelsouza@gmail.com>
2026-04-10 18:13:13 -0300

    fix: adicionando modulos necessários para o postgres no airflow

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -2,7 +2,7 @@ resource "google_compute_firewall" "allow_http" {
   name    = "allow-http"
   network = "default"
   allow {
-    ports    = ["80", "443", "8080"]
+    ports    = ["8080"] # Liberando a porta do Airflor (8080)
     protocol = "tcp"
   }
   source_ranges = ["0.0.0.0/0"]
--- terraform/vm.tf
+++ terraform/vm.tf
@@ -19,8 +19,8 @@ resource "google_compute_address" "static_ip" {
 
 resource "google_compute_instance" "default" {
   name         = "my-vm-teste"
-  machine_type = "e2-small"
-  zone         = "us-central1-a"
+  machine_type = "e2-medium"
+  zone         = "us-central1-b"
 
   tags = ["airflow-access"] # Tag para permitir acesso via firewall
 
@@ -39,16 +39,29 @@ resource "google_compute_instance" "default" {
 
   metadata_startup_script = <<-EOF
     #!/bin/bash
-    set -e  # Finaliza caso houver erro
+    set -e
+
+    # Instala Docker, Docker-Compose, Git e Curl
     apt-get update -y
-    apt-get install -y git curl docker.io docker-compose
+    apt-get install -y git curl docker.io docker-compose-v2
+    
     systemctl start docker
     systemctl enable docker
-    usermod -aG docker $(id -un)  # Add user to docker group
-    echo "Docker, Compose, and Git installed successfully!"
-    git clone https://github.com/dhiegoemmanuel2006/terraform-gcp /opt/project
+
+    # Prepara o diretório do projeto
+    mkdir -p /opt/project
+    chown -R ubuntu:ubuntu /opt/project # Ajusta dono para o usuário padrão
+    
+    # Clone do repositório
+    git clone https://github.com/dhiegoemmanuel2006/terraform-gcp /opt/project || echo "Repositório já existe"
+    
     cd /opt/project/airflow
+
+    # Garante que as pastas tenham permissões de escrita para o container
     mkdir -p dags logs plugins
+    chmod -R 777 dags logs plugins
+
+    # Sobe o Airflow
     docker compose up -d --build
   EOF
 }
